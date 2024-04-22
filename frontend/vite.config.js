d5c07899ac5fe3ca8e1064a5ea91397c878883e3
rohitlingarker <rohitlingerker@gmail.com>
2024-04-22 21:31:27 +0530

    deploying in render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "rohitlingarker",
+    project: "design-craft"
+  })],
+
   base: '/',
+
   server:{
     port: 3000,
+  },
+
+  build: {
+    sourcemap: true
   }
 })
